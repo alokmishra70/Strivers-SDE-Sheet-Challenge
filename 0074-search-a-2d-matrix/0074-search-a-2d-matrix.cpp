604b9ab823a96695e1ce98e1a3953bdb3b768a2e
Alok Kumar Mishra <89795561+alokmishra70@users.noreply.github.com>
2023-07-05 23:11:34 +0530

    Time: 6 ms (34.21%), Space: 9.4 MB (93.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // take a pointer at first row of the last element 
-        // THis is better solution          
+        // I use binary search but due to sapce complex. we don't  convert 2d mat to linear 
+        // we are using a index game 
+        // THis is optimal solution     
         int n = matrix.size();
         int m = matrix[0].size();
-        int i=0  , j = m-1 ;
-        while(i<n and j >= 0){
-            if(matrix[i][j] == target){
+        int low = 0  , high = m*n-1 ;
+        while(low <= high){
+            int mid = (low + (high - low)/2);
+            
+            if(matrix[mid/m][mid%m] == target){
                 return true;
             }
-            else if(matrix[i][j] > target){
-                j--;
+            else if(matrix[mid/m][mid%m] < target){
+                low = mid+1;
             }
             else{
-                i++;
+                high = mid-1;
             }
         }
         return false;
