9c086d1ea534bc07721c9f08ba7cdfc51f58f9e5
Alok Kumar Mishra <89795561+alokmishra70@users.noreply.github.com>
2023-07-06 01:04:06 +0530

    Time: 21 ms (15.98%), Space: 15.9 MB (27.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>v;
-        map<int,int>mp;
-        for(int i=0 ; i<n ;i++) mp[nums[i]]++;
-        for(auto x : mp){
-            if(x.second > n/3){
-                v.push_back(x.first);
+//         At most we got only two suxh numbers
+       int n = nums.size();
+       vector<int>ans;
+        int ele1 , ele2 ;
+        int cnt1 = 0 , cnt2 = 0;
+        
+        for(int i=0 ; i<n ; i++){
+            if(cnt1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        return v;
+        int c1 = 0 , c2 = 0 ;
+        for(int i=0 ; i<n ;i++){
+            if(ele1 == nums[i]) c1++;
+        }
+        if(c1 > n/3) ans.push_back(ele1);
+        for(int i=0 ; i<n ; i++){
+            if(ele2 == nums[i]) c2++;
+        }
+        if(c2 > n/3) ans.push_back(ele2);
+        
+        return ans;
+        
     }
 };
