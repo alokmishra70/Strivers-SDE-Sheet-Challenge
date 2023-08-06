f667df765c53480b3db2d5c80bb76dede4a73d40
Alok Kumar Mishra <89795561+alokmishra70@users.noreply.github.com>
2023-08-06 13:52:08 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -14,24 +14,18 @@ class Solution
         
         for(long long i=n-1; i>=0 ; i--){
             
+            
+                
+            while(!st.empty() and st.top() <= arr[i]){
+                st.pop();
+            }
             if(st.empty()){
                 ans.push_back(-1);
             }
-            else if(!st.empty() and st.top() > arr[i]){
+            else{
                 ans.push_back(st.top());
             }
-            else if(!st.empty() and st.top() <= arr[i]){
-                
-                while(!st.empty() and st.top() <= arr[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    ans.push_back(-1);
-                }
-                else{
-                    ans.push_back(st.top());
-                }
-            }
+            
             st.push(arr[i]);
         }
         reverse(ans.begin() , ans.end());
