244258ebe879e066ce424a41f570b90bc5c994a3
Alok Kumar Mishra <89795561+alokmishra70@users.noreply.github.com>
2023-10-17 12:51:26 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -9,16 +9,25 @@ using namespace std;
 // User function template for C++
 
 class Solution {
+    
   public:
+    int recursiveApproach(int arr[] , int low , int high , int k){
+        int mid = (low+high)/2;
+        
+        if(low > high) return -1;
+        if(arr[mid] == k) return mid;
+        else if(k>arr[mid]){
+           return recursiveApproach(arr,mid+1,high,k); 
+        } 
+        else {
+            return recursiveApproach(arr,low,mid-1,k);
+        }
+        
+    }
     int binarysearch(int arr[], int n, int k) {
         int low = 0 , high = n-1;
-        while( low <= high){
-            int mid = (low+high)/2;
-            if(arr[mid] == k) return mid;
-            else if (k < arr[mid]) high = mid - 1 ;
-            else low = mid + 1;
-        }
-        return -1;
+       
+        return recursiveApproach(arr,low,high,k);
     }
 };
 
